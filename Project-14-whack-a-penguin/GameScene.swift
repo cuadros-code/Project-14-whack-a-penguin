0a456143e88ddf26568159368e1e435900b68e97
kevin cuadros <kevindavidcuadros@gmail.com>
2024-09-14 14:58:30 -0500

    Initial Commit

---
+++ Project-14-whack-a-penguin/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project-14-whack-a-penguin
+//
+//  Created by Kevin Cuadros on 14/09/24.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+}
