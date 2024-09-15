950cad8b1f31ee3117281e3ba6dabeeb814a2aab
kevin cuadros <kevindavidcuadros@gmail.com>
2024-09-15 15:30:35 -0500

    Create Enemy

--- Project-14-whack-a-penguin/GameScene.swift
+++ Project-14-whack-a-penguin/GameScene.swift
@@ -9,6 +9,7 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var popupTime = 0.85
     var slots = [WhackSlot]()
     var gameScore: SKLabelNode!
     
@@ -20,17 +21,26 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "whackBackground")
-        background.position = CGPoint(x: 512, y: 390)
+        background.position = CGPoint(x: 512, y: 385)
         background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
         
         gameScore = SKLabelNode(fontNamed: "Chalkduster")
         gameScore.text = "Score: 0"
-        gameScore.position = CGPoint(x: 8, y: 20)
+        gameScore.position = CGPoint(x: 8, y: 15)
         gameScore.horizontalAlignmentMode = .left
         gameScore.fontSize = 46
         addChild(gameScore)
+        
+        for i in 0 ..< 5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 410)) }
+        for i in 0 ..< 4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 320)) }
+        for i in 0 ..< 5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 230)) }
+        for i in 0 ..< 4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 140)) }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.createEnemy()
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -43,6 +53,28 @@ class GameScene: SKScene {
         slot.configure(at: position)
         addChild(slot)
         slots.append(slot)
+    }
+    
+    func createEnemy(){
+        popupTime *= 0.991
+        
+        slots.shuffle()
+        slots[0].show(hideTime: popupTime)
+        
+        if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 8 { slots[2].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 10 { slots[3].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 11 { slots[4].show(hideTime: popupTime) }
+        
+        let minDelay = popupTime / 2.0
+        let maxDelay = popupTime * 2
+        let delay = Double.random(in: minDelay...maxDelay)
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+            self?.createEnemy()
+        }
+        
     }
     
 }
